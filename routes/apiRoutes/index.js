3c1260629d1294629c283a259c4aa16f6172f63b
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-12-16 21:29:42 -0800

    completed read write file system

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,23 +1,48 @@
 const router = require('express').Router();
 const { db } = require('../../db/db.json');
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
-    db
-    .getNotes()
-    .then(notes => res.json(notes))
-    .catch(err => res.status(500).json(err))
+    fs.readFile("./db/db.json", (err,data) => {
+        const notes = JSON.parse(data)
+        res.json(notes)
+    })
 });
 
 router.post("/notes", function (req, res) {
-    db
-    .addNote(req.body)
-    .then((notes) => res.json(notes))
-    .catch(err => res.status(500).json(err))
+    // read first
+    fs.readFile("./db/db.json", (err,data) => {
+        // parse
+        const notes = JSON.parse(data)
+        // assigning id
+        // ... spread operater
+        let newNote = {...req.body, id:uuidv4()}
+        notes.push(newNote)
+        fs.writeFile("./db/db.json", JSON.stringify(notes), () => {
+            res.json(req.body)
+        })
+    })
+
+
 });
 
-router.delete("/notes/:title", function (req, res) {
+
+//extra
+router.delete("/notes/:id", function (req, res) {
+    // fs.readfile
+    fs.readFile("./db/db.json", (err,data) => {
+        // parse data
+        const notes = JSON.parse(data)
+        // loop thru data and delete matching note on id
+        // resave w/ fs.write file the stringified notes
+        // when write file is done send back notes
+
+    // look at .then of delete in js front end
+    })
+
     db
-    .deleteNotes(req.params.title)
+    .deleteNotes(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch(err => res.status(500).json(err))
 });
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,15 +1,13 @@
 const router = require('express').Router();
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
-
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
+const path = require("path");
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
