b8f3a0d2b58c77604ad32738228da74a5e372e74
kimberlyamaya <kimberly.kimbell89@gmail.com>
2021-12-18 18:27:55 -0800

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const apiRoutes = require("./routes/apiRoutes/index");
 const htmlRoutes = require("./routes/htmlRoutes/index");
 
 const app = express()
-const port = process.env.port || 3002
+const port = process.env.PORT || 3002
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
